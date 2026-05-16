beb62bf9171fb88bd06a44abf7e1c3c085234ae0
rmoozo <rmzi.sharh@gmail.com>
2026-05-16 03:47:49 +0300

    revert download function

--- src/App.js
+++ src/App.js
@@ -44,7 +44,13 @@ function App() {
   };
 
  const downloadPdf = (url, name) => {
-  window.open(url, '_blank');
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = name;
+  link.target = '_blank';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 };
 
 
